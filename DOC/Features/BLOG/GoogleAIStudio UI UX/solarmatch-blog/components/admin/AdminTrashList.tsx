65fe441d96f7760d9ad8a4ab5e9821e17d14bcb8
Mohammad Ikramul Nayeem <mohammadikramul7@gmail.com>
2026-02-10 12:11:23 +0600

    Startup kit: DS-only, minimal homepage, all routes/pages removed

--- DOC/Features/BLOG/GoogleAIStudio UI UX/solarmatch-blog/components/admin/AdminTrashList.tsx
+++
@@ -1,4 +0,0 @@
-
-// This file is deprecated and no longer used.
-// Trash functionality has been moved to AdminPostList.tsx
-export default function AdminTrashList() { return null; }
--- src/app/dashboard/layout.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-import { DashboardShellClient } from "./_components/DashboardShellClient";
-
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <DashboardShellClient>{children}</DashboardShellClient>;
-}
